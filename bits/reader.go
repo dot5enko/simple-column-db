b5e336286f53eae7ed6626ddf632413b85a62bdf
Sergio <sergiydotsenko@gmail.com>
2025-12-11 14:02:36 +0100

    cleanup structure

--- bits/reader.go
+++ bits/reader.go
@@ -3,85 +3,108 @@ package bits
 import (
 	"encoding/binary"
 	"errors"
+	"io"
 	"math"
 	"unsafe"
 
 	"github.com/google/uuid"
 )
 
-var ErrEOF = errors.New("end of file")
+var (
+	ErrEOF          = errors.New("end of file")
+	ErrReadMismatch = errors.New("read size mismatch")
+)
 
-type Reader struct {
-	buf   []byte
-	pos   int
+const MaxBinReaderBufferSize = 256
+
+type BitsReader struct {
+	readBuffer [MaxBinReaderBufferSize]byte
+
+	buf   io.Reader
 	order binary.ByteOrder
 }
 
-func NewBinReader(buf []byte, order binary.ByteOrder) *Reader {
-	return &Reader{buf: buf, order: order}
+func NewReader(buf io.Reader, order binary.ByteOrder) *BitsReader {
+	return &BitsReader{buf: buf, order: order}
 }
 
-func (r *Reader) remaining(n int) bool {
-	return r.pos+n <= len(r.buf)
-}
+func (r *BitsReader) readNextBytesIntoReadBuffer(size int) error {
+	readBytes, err := r.buf.Read(r.readBuffer[:size])
 
-func (r *Reader) ReadU8() (uint8, error) {
-	if !r.remaining(1) {
-		return 0, ErrEOF
+	if err != nil {
+		return err
 	}
-	v := r.buf[r.pos]
-	r.pos++
-	return v, nil
+
+	if readBytes != size {
+		return ErrReadMismatch
+	}
+
+	return nil
 }
 
-func (r *Reader) ReadI8() (int8, error) {
+func (r *BitsReader) ReadU8() (uint8, error) {
+	err := r.readNextBytesIntoReadBuffer(1)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return r.readBuffer[0], err
+}
+
+func (r *BitsReader) ReadI8() (int8, error) {
 	u, err := r.ReadU8()
 	return int8(u), err
 }
 
-func (r *Reader) ReadU16() (uint16, error) {
-	if !r.remaining(2) {
-		return 0, ErrEOF
+func (r *BitsReader) ReadU16() (uint16, error) {
+
+	err := r.readNextBytesIntoReadBuffer(2)
+
+	if err != nil {
+		return 0, err
 	}
-	v := r.order.Uint16(r.buf[r.pos:])
-	r.pos += 2
-	return v, nil
+
+	v := r.order.Uint16(r.readBuffer[:2])
+	return v, err
 }
 
-func (r *Reader) ReadI16() (int16, error) {
+func (r *BitsReader) ReadI16() (int16, error) {
 	v, err := r.ReadU16()
 	return int16(v), err
 }
 
-func (r *Reader) ReadUUID() (result uuid.UUID, err error) {
+func (r *BitsReader) ReadUUID() (result uuid.UUID, err error) {
 	err = r.ReadBytes(16, result[:])
 	return result, err
 }
 
-func (r *Reader) ReadU32() (uint32, error) {
-	if !r.remaining(4) {
-		return 0, ErrEOF
+func (r *BitsReader) ReadU32() (uint32, error) {
+	readErr := r.readNextBytesIntoReadBuffer(4)
+	if readErr != nil {
+		return 0, readErr
 	}
-	v := r.order.Uint32(r.buf[r.pos:])
-	r.pos += 4
+	v := r.order.Uint32(r.readBuffer[:4])
 	return v, nil
 }
 
-func (r *Reader) ReadI32() (int32, error) {
+func (r *BitsReader) ReadI32() (int32, error) {
 	v, err := r.ReadU32()
 	return int32(v), err
 }
 
-func (r *Reader) ReadU64() (uint64, error) {
-	if !r.remaining(8) {
-		return 0, ErrEOF
+func (r *BitsReader) ReadU64() (uint64, error) {
+
+	readErr := r.readNextBytesIntoReadBuffer(8)
+	if readErr != nil {
+		return 0, readErr
 	}
-	v := r.order.Uint64(r.buf[r.pos:])
-	r.pos += 8
+
+	v := r.order.Uint64(r.readBuffer[:8])
 	return v, nil
 }
 
-func (r *Reader) MustReadU64() uint64 {
+func (r *BitsReader) MustReadU64() uint64 {
 	u, er := r.ReadU64()
 	if er != nil {
 		panic(er)
@@ -89,12 +112,12 @@ func (r *Reader) MustReadU64() uint64 {
 	return u
 }
 
-func (r *Reader) ReadI64() (int64, error) {
+func (r *BitsReader) ReadI64() (int64, error) {
 	v, err := r.ReadU64()
 	return int64(v), err
 }
 
-func (r *Reader) MustReadI64() int64 {
+func (r *BitsReader) MustReadI64() int64 {
 	i, er := r.ReadI64()
 	if er != nil {
 		panic(er)
@@ -103,7 +126,7 @@ func (r *Reader) MustReadI64() int64 {
 
 }
 
-func (r *Reader) ReadF32() (float32, error) {
+func (r *BitsReader) ReadF32() (float32, error) {
 	u, err := r.ReadU32()
 	if err != nil {
 		return 0, err
@@ -111,7 +134,7 @@ func (r *Reader) ReadF32() (float32, error) {
 	return math.Float32frombits(u), nil
 }
 
-func (r *Reader) ReadF64() (float64, error) {
+func (r *BitsReader) ReadF64() (float64, error) {
 	u, err := r.ReadU64()
 	if err != nil {
 		return 0, err
@@ -119,7 +142,7 @@ func (r *Reader) ReadF64() (float64, error) {
 	return math.Float64frombits(u), nil
 }
 
-func (r *Reader) MustReadF64() float64 {
+func (r *BitsReader) MustReadF64() float64 {
 	f, er := r.ReadF64()
 	if er != nil {
 		panic(er)
@@ -127,13 +150,15 @@ func (r *Reader) MustReadF64() float64 {
 	return f
 }
 
-func (r *Reader) ReadBytes(n int, out []byte) error {
-	if !r.remaining(n) {
-		return ErrEOF
+func (r *BitsReader) ReadBytes(n int, out []byte) error {
+
+	readBytes, err := r.buf.Read(out[:n])
+
+	if readBytes != n {
+		return ErrReadMismatch
 	}
-	copy(out, r.buf[r.pos:r.pos+n])
-	r.pos += n
-	return nil
+
+	return err
 }
 
 func MapBytesToInt[T any](data []byte, count int) *T {
--- block/disk_header.go
+++ block/disk_header.go
@@ -1,6 +1,11 @@
 package block
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/dot5enko/simple-column-db/bits"
 	"github.com/dot5enko/simple-column-db/schema"
 	"github.com/google/uuid"
 )
@@ -26,4 +31,48 @@ type DiskHeader struct {
 
 	// reserved for future use
 	Reserved [ReservedSize]uint8
+}
+
+func (header *DiskHeader) FromBytes(input []byte) (topErr error) {
+
+	reader := bits.NewReader(bytes.NewBuffer(input), binary.LittleEndian)
+
+	header.GroupUid, topErr = reader.ReadUUID()
+	if topErr != nil {
+		return fmt.Errorf("unable to decode block header guid: %s", topErr.Error())
+	}
+
+	header.StartOffset, topErr = reader.ReadU64()
+	if topErr != nil {
+		return fmt.Errorf("unable to decode block header start offset: %s", topErr.Error())
+	}
+	header.CompressedSize, topErr = reader.ReadU64()
+	if topErr != nil {
+		return fmt.Errorf("unable to decode block header compressed size: %s", topErr.Error())
+	}
+
+	columnTypeRaw, topErr := reader.ReadU8()
+	if topErr != nil {
+		return fmt.Errorf("unable to decode block header column type: %s", topErr.Error())
+	}
+
+	columnType := schema.FieldType(columnTypeRaw)
+
+	// read max/min values
+	switch columnType {
+	case schema.Int8FieldType, schema.Int16FieldType, schema.Int32FieldType, schema.Int64FieldType:
+		header.MaxIValue = reader.MustReadI64()
+		header.MinIValue = reader.MustReadI64()
+	case schema.Uint8FieldType, schema.Uint16FieldType, schema.Uint32FieldType, schema.Uint64FieldType:
+		header.MaxIValue = int64(reader.MustReadU64())
+		header.MinIValue = int64(reader.MustReadU64())
+	case schema.Float32FieldType, schema.Float64FieldType:
+		header.MaxFValue = reader.MustReadF64()
+		header.MinFValue = reader.MustReadF64()
+	default:
+		panic(fmt.Sprintf("unsupported field type: %d", columnType.String()))
+	}
+
+	return nil
+
 }
