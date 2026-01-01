7d58d47b2fd73b5b3a4e1d211c3b13fd7853dac2
Sergio <sergiydotsenko@gmail.com>
2026-01-01 18:55:06 +0100

    sort blocks

---
+++ manager/meta/load_block_data.go
@@ -0,0 +1,99 @@
+package meta
+
+import (
+	"fmt"
+	"slices"
+	"time"
+
+	"github.com/dot5enko/simple-column-db/manager/cache"
+	"github.com/dot5enko/simple-column-db/ops"
+	"github.com/dot5enko/simple-column-db/schema"
+	"github.com/google/uuid"
+)
+
+func UniqueCountSorted[T ops.NumericTypes](xs []T) int {
+	if len(xs) == 0 {
+		return 0
+	}
+	slices.Sort(xs)
+
+	cnt := 1
+	for i := 1; i < len(xs); i++ {
+		if xs[i] != xs[i-1] {
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func (m *SlabManager) LoadBlockToRuntimeBlockData(
+	schemaObject schema.Schema,
+	slab *schema.DiskSlabHeader,
+	block uuid.UUID,
+) (*schema.RuntimeBlockData, error) {
+
+	cached := m.getBlockFromCache(slab.Uid, block)
+
+	if cached != nil {
+		return cached.runtime, nil
+	} else {
+		// put into cache
+
+		var blockHeader schema.DiskHeader
+		blockIdx := -1
+		blockStartOffset := 0
+
+		for idx, it := range slab.BlockHeaders {
+			if it.Uid == block {
+				blockHeader = it
+				blockIdx = idx
+				break
+			}
+		}
+
+		if blockIdx < 0 {
+			return nil, fmt.Errorf("block you are looking for (%s) not found in slab %s", block.String(), slab.Uid.String())
+		} else {
+
+			blockSize := blockHeader.DataType.BlockSize()
+			blockStartOffset = blockIdx * blockSize
+
+			slabData := m.getSlabDataFromCache(slab.Uid)
+			if slabData == nil {
+				_, loadSlabErr := m.LoadSlabDataContents(&schemaObject, slab.Uid)
+				if loadSlabErr != nil {
+					return nil, loadSlabErr
+				}
+				slabData = m.getSlabDataFromCache(slab.Uid)
+				if slabData == nil {
+					panic("cache should be loaded by now, probably out of memory?")
+				}
+			}
+
+			blockRawData := slabData.Data[blockStartOffset:]
+			runtimeBlockData, runtimeDecodeErr := DecodeRawBlockData(blockRawData, &blockHeader)
+
+			if runtimeDecodeErr != nil {
+				return nil, fmt.Errorf("unable to decoded raw block data for slab %s. block %s: %s", slab.Uid.String(), block.String(), runtimeDecodeErr.Error())
+			} else {
+				m.rt.locker.Lock()
+				defer m.rt.locker.Unlock()
+
+				blockId := GetUniqueBlockId(slab.Uid, block)
+
+				// slog.Info("cache entry put", "entry_id", slabData.RtStats.CacheEntryId)
+
+				m.rt.cache[blockId] = BlockCacheItem{
+					header:  &blockHeader,
+					runtime: runtimeBlockData,
+					rtStats: &cache.CacheStats{CacheEntryId: slabData.RtStats.CacheEntryId, Created: time.Now(), Reads: 1},
+				}
+
+				return runtimeBlockData, nil
+			}
+
+		}
+
+	}
+
+}
--- manager/meta/slab_manager.go
+++ manager/meta/slab_manager.go
@@ -205,77 +205,6 @@ func (m *SlabManager) getBlockFromCache(slab, block uuid.UUID) *BlockCacheItem {
 }
 
 // load block from slab
-func (m *SlabManager) LoadBlockToRuntimeBlockData(
-	schemaObject schema.Schema,
-	slab *schema.DiskSlabHeader,
-	block uuid.UUID,
-) (*schema.RuntimeBlockData, error) {
-
-	cached := m.getBlockFromCache(slab.Uid, block)
-
-	if cached != nil {
-		return cached.runtime, nil
-	} else {
-		// put into cache
-
-		var blockHeader schema.DiskHeader
-		blockIdx := -1
-		blockStartOffset := 0
-
-		for idx, it := range slab.BlockHeaders {
-			if it.Uid == block {
-				blockHeader = it
-				blockIdx = idx
-				break
-			}
-		}
-
-		if blockIdx < 0 {
-			return nil, fmt.Errorf("block you are looking for (%s) not found in slab %s", block.String(), slab.Uid.String())
-		} else {
-
-			blockSize := blockHeader.DataType.BlockSize()
-			blockStartOffset = blockIdx * blockSize
-
-			slabData := m.getSlabDataFromCache(slab.Uid)
-			if slabData == nil {
-				_, loadSlabErr := m.LoadSlabDataContents(&schemaObject, slab.Uid)
-				if loadSlabErr != nil {
-					return nil, loadSlabErr
-				}
-				slabData = m.getSlabDataFromCache(slab.Uid)
-				if slabData == nil {
-					panic("cache should be loaded by now, probably out of memory?")
-				}
-			}
-
-			blockRawData := slabData.Data[blockStartOffset:]
-			runtimeBlockData, runtimeDecodeErr := DecodeRawBlockData(blockRawData, &blockHeader)
-
-			if runtimeDecodeErr != nil {
-				return nil, fmt.Errorf("unable to decoded raw block data for slab %s. block %s: %s", slab.Uid.String(), block.String(), runtimeDecodeErr.Error())
-			} else {
-				m.rt.locker.Lock()
-				defer m.rt.locker.Unlock()
-
-				blockId := GetUniqueBlockId(slab.Uid, block)
-
-				// slog.Info("cache entry put", "entry_id", slabData.RtStats.CacheEntryId)
-
-				m.rt.cache[blockId] = BlockCacheItem{
-					header:  &blockHeader,
-					runtime: runtimeBlockData,
-					rtStats: &cache.CacheStats{CacheEntryId: slabData.RtStats.CacheEntryId, Created: time.Now(), Reads: 1},
-				}
-
-				return runtimeBlockData, nil
-			}
-
-		}
-
-	}
-
-}
 
 // return RuntimeBlockData
 func DecodeRawBlockData(blockData []byte, bheader *schema.DiskHeader) (*schema.RuntimeBlockData, error) {
@@ -295,6 +224,14 @@ func DecodeRawBlockData(blockData []byte, bheader *schema.DiskHeader) (*schema.R
 	case schema.Uint64FieldType:
 
 		result := bits.MapBytesToArray[uint64](blockData, schema.BlockRowsSize)
+
+		// // sort ?
+		// sortStart := time.Now()
+		// uniqueItems := UniqueCountSorted(result)
+		// sortTook := time.Since(sortStart).Seconds()
+
+		// fmt.Printf(" --- sort [%s] took %.2fms: unique : %d\n", bheader.DataType.String(), sortTook*1000, uniqueItems)
+
 		runtimeData = schema.NewRuntimeBlockDataFromSlice(result, int(bheader.Items))
 
 	case schema.Uint8FieldType:
